3785a9d092a3981ce462c4d84bfa58e7c2de75ce
AntoDono <anthony20151128@gmail.com>
2023-06-18 12:51:48 -0400

    error page

--- components/menu.vue
+++ components/menu.vue
@@ -4,6 +4,22 @@
     </div>
 </template>
 
+<script setup>
+import { ref, onMounted, reactive } from 'vue'
+import { useFetch } from 'nuxt/app';
+
+
+onMounted(async()=>{
+    const env = useRuntimeConfig()
+    const { data, pending, error, refresh} = await useFetch(`${env.public.api}/`, { crossOrigin: '*' })
+
+    if (error.value && window.location.pathname.indexOf("/server_is_down") == -1){
+        window.location.replace("/server_is_down")
+    }
+})
+
+</script>
+
 <style scoped>
 #menu{
     width: 20vw;
---
+++ pages/server_is_down.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full h-screen bg-accent1">
+        <h1 class="text-[5vmin] font-[Montserrat] text-secondary">
+            Server is currently offline.
+        </h1>
+        <h2 class="text-[3vmin] font-[Montserrat] text-secondary">
+            If this is a mistake, contact anthony20151128@gmail.com
+        </h2>
+        <h3 class="text-[2vmin] font-[Montserrat] text-secondary">
+            Sorry for the inconvenience.
+        </h3>
+    </div>
+</template>
