7f7d11d1d5ecd1a80947871f6c55a26ffac27d86
AntoDono <anthony20151128@gmail.com>
2023-07-18 03:47:31 -0400

    adjusted speed to 150 ms

--- components/chatbox.vue
+++ components/chatbox.vue
@@ -105,7 +105,7 @@ onMounted(async()=>{
             if (i != 0) messages[messages.length - 1].message += " "
             messages[messages.length - 1].message += word
 
-            sleep(100)
+            sleep(150)
 
             scroll_down()
         }
