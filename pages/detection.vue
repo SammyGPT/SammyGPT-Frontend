7f5ea727454bc91493caccdd344fec232ea8f912
MNRMax <maximilian.shabatayev@gmail.com>
2023-07-25 12:58:24 -0400

    -

---
+++ pages/detection.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-[100vw] h-[100vh] dark:bg-background bg-accent2">
+        <h1 class="text-black dark:text-white text-center text-[3rem] font-[M]">AI Detector</h1>
+        <div class="flex justify-center w-full">
+            <textarea class="bg-light-background dark:bg-primary mx-auto w-[50vw] resize-none p-4 text-[1rem]" rows="20">
+                
+            </textarea>
+        </div>
+    </div>
+</template>
