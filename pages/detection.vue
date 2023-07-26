d407277ad39199c9426b60a6cd1cfeeb5cb0aeac
AntoDono <anthony20151128@gmail.com>
2023-07-26 18:15:47 -0400

    ai checking finished:

--- pages/detection.vue
+++ pages/detection.vue
@@ -4,27 +4,30 @@ import axios from 'axios';
 
 const textarea = ref(null)
 const result = ref("")
+const confidence = ref("")
 
 async function getResults() {
     if (textarea.value.value == "") {return}
     const response = await axios.post('http://localhost:5000/detect', textarea.value.value);
     const data = JSON.parse(response.data)
-    result.value = `${Math.round(data.score*10000)/100}% Result: ${data.label}`
+    result.value = `Result: ${data.label}`
+    confidence.value = `Confidence: ${Math.round(data.score*10000)/100}%`
     console.log(result.value)
 }
 </script>
 
 <template>
-    <div class="w-[100vw] h-[100vh] dark:bg-background bg-slate-100 font-[Montserrat] p-8">
+    <div class="w-[100vw] h-[100vh] dark:bg-primary bg-slate-100 font-[Montserrat] p-8">
         <h1 class="text-black dark:text-white text-center text-[3rem]">AI Detector</h1>
         <div class="flex justify-center w-full mt-9">
             <textarea
-                class="bg-light-background dark:bg-primary mx-auto w-[50vw] dark:text-white text-black resize-none p-4 text-[1rem] rounded-xl"
+                class="bg-light-background dark:bg-slate-700 mx-auto w-[50vw] dark:text-white text-black resize-none p-4 text-[1rem] rounded-xl"
                 rows="20" placeholder="Enter your text" ref="textarea"></textarea>
         </div>
         <div class="flex justify-center my-8">
             <Button :toggle_button="false" :selected="false" text="Check for AI" @clicked="getResults" class=""></Button>
         </div>
-        <h2 class="text-black dark:text-white text-center text-[3rem]">{{ result }}</h2>
+        <h2 class="text-black dark:text-white text-center text-[2rem]">{{ result }}</h2>
+        <h2 class="text-black dark:text-white text-center text-[2rem]">{{ confidence }}</h2>
     </div>
 </template>
