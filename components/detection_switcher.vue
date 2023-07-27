6597f6b35a9c8459f05d3e5df9797d1c37a5d3d1
Maximilian Shabatayev <88982841+MNRMax@users.noreply.github.com>
2023-07-27 08:47:43 -0400

    Update detection_switcher.vue

--- components/detection_switcher.vue
+++ components/detection_switcher.vue
@@ -26,7 +26,7 @@ async function handleClick(path) {
 
 <template>
     <div id="container" class="flex flex-col absolute w-[10vw] left-[2vw] top-[3vw] gap-1">
-        <h3 class=" bg-transparent text-white text-center text-[1.5rem]">Change Mode</h3>
+        <h3 class=" bg-transparent darK:text-white text-black text-center text-[1.5rem]">Change Mode</h3>
         <Button text="Text" :selected="normal" :toggle_button="true" :non-clickable="normal" @clicked="handleClick('/detection')"></Button>
         <Button text="Upload File" :selected="files" :toggle_button="true" :non-clickable="files" @clicked="handleClick('/detection_files')"></Button>
         <!-- <Button v-if="update" text="Import From Drive" :selected="drive" :toggle_button="true" :non-clickable="drive" @clicked="handleClick('/detection_drive')"></Button> -->
