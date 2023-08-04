a12ffe246640442eafe7d38b544ee3758ebf8f18
MNRMax <maximilian.shabatayev@gmail.com>
2023-08-04 17:47:19 -0400

    google login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'], 
+  modules: ['@nuxtjs/tailwindcss','nuxt-vue3-google-signin'], 
+  googleSignIn: {
+    clientId: '512779933430-oo9c90p46eum9l21goil03hul7eso364.apps.googleusercontent.com',
+  },
   css: ['~/assets/css/font.css', '~/assets/css/base.css'],
   ssr: false,
   runtimeConfig: {
