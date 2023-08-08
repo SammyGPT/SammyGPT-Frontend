0a47ddbed223c69551a5e2f1341fb34f28d3f81f
MNRMax <maximilian.shabatayev@gmail.com>
2023-08-07 23:33:35 -0400

    added picker

--- pages/detection_drive.vue
+++ pages/detection_drive.vue
@@ -1,69 +1,123 @@
 <script setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 import axios from 'axios';
-import { useTokenClient } from "vue3-google-signin"
 
 const textarea = ref(null)
 const result = ref("")
 const progress = ref(0)
 const results = ref([])
+let tokenClient;
+let accessToken = null;
+let pickerInited = false;
+let gisInited = false;
+
+const i18n = useI18n()
+
 const env = useRuntimeConfig()
 
-async function handleFile(e) {
-    results.value = []
-    progress.value = 0
-    var files = e.target.files || e.dataTransfer.files;
-    if (!files.length)
-        return;
-    for (let i = 0; i < files.length; i++){
-        var formData = new FormData();
-        formData.append("file", files[i]);
-        const response = await axios.post(`${env.public.ws_protocol}://${env.public.api}upload`, formData)
-        const data = await JSON.parse(response.data)
-        results.value.push({
-            fileName: files[i].name,
-            result: data
-        })
-        progress.value = Math.round((i+1)/files.length * 100)
-    }
-    console.log(results.value)
-}
-
 const handleOnSuccess = (response) => {
-  console.log("Access Token: " + response.access_token);
+    console.log("Access Token: " + response.access_token);
+    createPicker(response.access_token)
 };
 
 const handleOnError = (errorResponse) => {
-  console.log("Error: " + errorResponse);
+    console.log("Error: " + errorResponse);
 };
 
 const { isReady, login } = useTokenClient({
-  onSuccess: handleOnSuccess,
-  onError: handleOnError,
-  // other options
+    onSuccess: handleOnSuccess,
+    onError: handleOnError,
+    // other options
 });
+
+onMounted(() => {
+    let gDrive = document.createElement("script");
+    gDrive.setAttribute("type", "text/javascript");
+    gDrive.setAttribute("src", "https://apis.google.com/js/api.js");
+    document.head.appendChild(gDrive);
+    let gsi = document.createElement("script");
+    gsi.setAttribute("type", "text/javascript");
+    gsi.setAttribute("src", "https://apis.google.com/js/api.js");
+    document.head.appendChild(gsi);
+})
+
+function init() {
+    onApiLoad()
+    gisLoaded()
+    createPicker()
+}
+
+// Use the API Loader script to load google.picker
+function onApiLoad() {
+    gapi.load('picker', onPickerApiLoad);
+}
+
+function onPickerApiLoad() {
+    pickerInited = true;
+}
+
+function gisLoaded() {
+    // TODO(developer): Replace with your client ID and required scopes.
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: '885744489083-sesf2bhp70vs7cgqsl7mrgg5mi4l7s6k.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/drive',
+        callback: '', // defined later
+    });
+    gisInited = true;
+}
+
+
+function createPicker() {
+    if (accessToken === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({ prompt: '' });
+    }
+    tokenClient.callback = async (response) => {
+        if (response.error !== undefined) {
+            throw (response);
+        }
+        accessToken = response.access_token;
+
+        const picker = new google.picker.PickerBuilder()
+            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+            .addView(google.picker.ViewId.DOCUMENTS)
+            .setOAuthToken(accessToken)
+            .setLocale(i18n.locale.value == "zh" ? "zh-CN" : i18n.locale.value)
+            .setDeveloperKey('AIzaSyA-xKIMopdn_y1Vr8M9v9hTC0ypPd6yDy0')
+            .setCallback(pickerCallback)
+            .build();
+        picker.setVisible(true);
+    };
+}
+
+async function pickerCallback(data) {
+    console.log("PickerCallback", data);
+}
 </script>
 
 <template>
     <div class="w-full min-w-[100vw] h-full min-h-[100vh] dark:bg-background bg-slate-100 font-[Montserrat] p-8">
         <h1 class="text-black dark:text-white text-center text-[3rem]">AI Detector</h1>
         <div class="mx-auto w-fit mt-9">
-            <input class="dark:text-white text-black text-center" type="file" accept=".doc, .docx, .DOT, .PDF, .CSV, .TXT, .XLS, .XLSX, .JSON" @change="handleFile" multiple="multiple">
+            <button :disabled="!isReady" @click="() => init()" class="text-white">Upload Files From Drive</button>
         </div>
         <div class="flex justify-center my-8">
             <Button :toggle_button="false" :selected="false" text="Check for AI" @clicked="getResults" class=""></Button>
-            <button :disabled="!isReady" @click="() => login()" class="text-white">Login with Google</button>
         </div>
         <!-- <h2 class="text-black dark:text-white text-center text-[3rem]">{{ result }}</h2> -->
-        <DetectionLoading :progress="progress"/>
+        <DetectionLoading :progress="progress" />
         <div id="files" class="flex flex-wrap mt-[15vh] h-auto">
             <div class="w-[20vw] h-[20vw] bg-white" v-for="result in results">
                 <h3 class="text-center text-[2rem]">{{ result.fileName }}</h3>
-                <p class="text-center text-[1.2rem]">{{ `Made by ${result.result.result}, ${Math.round(result.result.probability*10000)/100}% confidence` }}</p>
+                <p class="text-center text-[1.2rem]">{{ `Made by ${result.result.result},
+                                    ${Math.round(result.result.probability * 10000) / 100}% confidence` }}</p>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
