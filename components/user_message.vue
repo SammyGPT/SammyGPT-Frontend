927cba47de87eb1cc37555922929347af8eaac88
MNRMax <maximilian.shabatayev@gmail.com>
2023-08-30 21:10:20 -0400

    fix pfp

--- components/user_message.vue
+++ components/user_message.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="message" class="max-w-full dark:bg-accent1 bg-accent2 rounded-xl dark:text-secondary text-primary text-[Montserrat] flex items-center justify-start">
         <div class="flex" id="bot_image">
-            <img src="~/assets/images/user_image.png" class="w-12 h-12 rounded-full"/>
+            <img :src="pfp" class="w-12 h-12 rounded-full"/>
         </div>
         <p id="content" class="ease-in-out duration-300 dark:text-white text-black">
             {{ props.message }}
@@ -13,11 +13,19 @@
 <script setup>
 
 import { toRefs, toRef, defineProps } from 'vue'
+const userDataStore = useState('userData', () => null)
+
 
 const props = defineProps({
     message: { type:String, required: true}
 })
 
+const pfp = computed(() => {
+    if (userDataStore.value == null) {
+        return "user_image.png"
+    } 
+    return userDataStore.value.picture
+})
 </script>
 
 <style scoped>
