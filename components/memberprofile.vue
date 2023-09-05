e726b6be52854d346d1afff953bc7fe171ade24e
bridgetf7 <fbridget0404@gmail.com>
2023-09-04 21:08:39 -0400

    basics done

---
+++ components/memberprofile.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="team-profile">
+        <img :src="img" alt="Profile Picture" class="w-300 h-300 rounded-full" />
+        <div class="title">{{ title }}</div>
+        <div class="name">{{ name }}</div>
+        <div class="description">{{ description }}</div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    img: {
+        type: Image,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+})
+</script>
---
+++ pages/team.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="">Our Team</h1>
+    <div class="profiles">
+        <div class="" v-for="(profile, index) in profiles" :key="index">
+            <Memberprofile :img="profile.img" :title="profile.title" :name="profile.name"
+                :description="profile.description"></Memberprofile>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Memberprofile from '../components/memberprofile.vue'
+import { profiles } from '../assets/team.js'
+</script>
