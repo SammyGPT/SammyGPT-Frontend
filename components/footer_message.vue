d4b1e3a1eb617b54082d1b9e05bbdfacf8b9a037
AntoDono <anthony20151128@gmail.com>
2023-09-04 23:17:13 -0400

    removed credits on the chat

--- components/footer_message.vue
+++ components/footer_message.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="flex flex-col items-center justify-center w-full fixed bottom-0 p-2">
         <h3 class="dark:text-secondary text-primary font-[Montserrat] text-[1.5vmin]">{{ $t("chat-copy-right") }}</h3>
-        <h3  class="dark:text-secondary text-primary font-[Montserrat] text-[1.5vmin]">{{ $t("chat-credits") }}</h3>
     </div>
 </template>
