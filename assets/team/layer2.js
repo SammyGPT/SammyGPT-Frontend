0792efda8a833ec7312b09ab4e5ded459a8c47cc
bridgetf7 <fbridget0404@gmail.com>
2023-10-09 14:14:28 -0400

    translationssss

--- assets/team/layer2.js
+++ assets/team/layer2.js
@@ -1,16 +1,48 @@
 export const layer2 = [
     {
-        img: '/team/max.jpg',
-        title: 'Systems Specialist',
-        name: 'Maximilian Shabatayev',
-        description: 'Database Management, Server Management, Web Development, Data Analysis, API Development and Integration',
-        message: "The seagull on the frontpage took me 3 hours"
+        en: {
+            img: '/team/max.jpg',
+            title: 'Systems Specialist',
+            name: 'Maximilian Shabatayev',
+            description: 'Database Management, Server Management, Web Development, Data Analysis, API Development and Integration',
+            message: "The seagull on the frontpage took me 3 hours"
+        },
+        zh: {
+            img: '/team/max.jpg',
+            title: '系统专家',
+            name: 'Maximilian Shabatayev',
+            description: '数据库管理，服务器管理，Web开发，数据分析，API开发与集成',
+            message: '主页上的海鸥花了我3个小时'
+        },
+        ru: {
+            img: '/team/max.jpg',
+            title: 'Специалист по системам',
+            name: 'Maximilian Shabatayev',
+            description: 'Управление базами данных, Управление серверами, Веб-разработка, Анализ данных, Разработка и интеграция API',
+            message: 'Чайка на главной странице заняла у меня 3 часа'
+        }
     },
     {
-        img: '/team/bridget.jpg',
-        title: 'UI Specialist',
-        name: 'Bridget Feng',
-        description: 'User Interface Design, User Research & Experience, Responsive Web Design, Prototyping, Spell Check',
-        message: "The seagull enhances user experience"
+        en: {
+            img: '/team/bridget.jpg',
+            title: 'UI Specialist',
+            name: 'Bridget Feng',
+            description: 'User Interface Design, User Research & Experience, Responsive Web Design, Prototyping, Spell Check',
+            message: "The seagull enhances user experience"
+        },
+        zh: {
+            img: '/team/bridget.jpg',
+            title: 'UI 专家',
+            name: 'Bridget Feng',
+            description: '用户界面设计，用户研究与体验，响应式网页设计，原型设计，拼写检查',
+            message: '海鸥提升了用户体验'
+        },
+        ru: {
+            img: '/team/bridget.jpg',
+            title: 'Специалист по интерфейсу',
+            name: 'Bridget Feng',
+            description: 'Дизайн пользовательского интерфейса, Исследование и опыт пользователей, Адаптивный дизайн веб-интерфейса, Прототипирование, Проверка орфографии',
+            message: 'Чайка улучшает пользовательский опыт'
+        }
     },
-]
+];
--- assets/team/special.js
+++ assets/team/special.js
@@ -1,9 +1,23 @@
 export const special = [
     {
-        img: '/team/whalen.jpg',
-        title: 'Computer Science Teacher',
-        name: 'Michael Whalen',
-        description: 'Web Development, Developing functional applications, Our Lord and Savior',
-        message: "🐋"
-    },
+        en: {
+            img: '/team/whalen.jpg',
+            title: 'Computer Science Teacher',
+            name: 'Michael Whalen',
+            description: 'Web Development, Developing functional applications, Our Lord and Savior',
+            message: "🐋"
+        },     zh: {
+            img: '/team/whalen.jpg',
+            title: '计算机科学教师',
+            name: 'Michael Whalen',
+            description: 'Web开发，开发功能性应用程序，我们的主和救世主',
+            message: '🐋'
+        },     ru: {
+            img: '/team/whalen.jpg',
+            title: 'Учитель информатики',
+            name: 'Michael Whalen',
+            description: 'Веб-разработка, Разработка функциональных приложений, Наш Господь и Спаситель',
+            message: '🐋'
+        }
+    }
 ]
